50f6450f198838d5de8430c2424c6b6ba79630a4
Giacomo Castellani <yasu83@gmail.com>
2022-09-16 11:51:22 +0200

    added variables section and used in before/after/parameters/asserts sections / added step section to group many commands/test results related to same test (#4)
    Co-authored-by: Riccardo Sale <riccardo.sale@posteitaliane.it>

--- src/TestBase/TestCommand.cs
+++ src/TestBase/TestCommand.cs
@@ -5,13 +5,6 @@ namespace TestBase
     public class TestCommand
     {
         public string CommandText { get; set; }
-        public List<TestCommandParameter>? Parameters { get; set; }
-    }
-
-    public class TestCommandParameter
-    {
-        public string Name { get; set; }
-        public SqlDbType Type { get; set; }
-        public object? Value { get; set; }
+        public Dictionary<string, object>? Parameters { get; set; }
     }
 }
